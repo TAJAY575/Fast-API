4fb61b2246ea7a68a98868fc4455af3cffdee547
Tajay <tajaydrum575@gmail.com>
2025-03-16 21:50:07 -0400

    Added PostgreSQL Database Connection and CRUD Operations - Integrated PostgreSQL connection using psycopg2. - Updated code to connect to the database and handle posts CRUD operations. - Replaced `my_posts` list with database interactions for `GET`, `POST`, `PUT`, and `DELETE` endpoints. - Added database connection retry logic to handle connection failures. - Configured environment variables for database credentials (`user`, `password`). - Improved error handling for non-existent posts during update and delete operations. - Modify server start-up exception handling.

--- app/main.py
+++ app/main.py
@@ -4,30 +4,34 @@ from fastapi.params import Body
 from pydantic import BaseModel 
 from random import randrange 
 import uvicorn 
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+import os
 app = FastAPI()
+
+
+user= os.getenv("user")
+password= os.getenv("password")
 
 class Post(BaseModel):
     title: str
     content: str
     published : bool = True
-    rating : Optional[int] = None 
     
-    
-my_posts = [{"title" : "title of post 1" , "content": "content of post 1", "id" : 1}, 
-            {"title" : "favorite foods", "content" : "i like pizza", "id" : 2}]
-    
-    
-def  find_post(id):
-    for p in my_posts : 
-        if p["id"] == id : 
-            return p  
 
-def find_index_post(id) :
-    for i , p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
+while  True:
+    try : 
+        conn = psycopg2.connect(host= 'localhost', database ='postgres', user = user,
+                                password = password, cursor_factory = RealDictCursor)
+        cursor = conn.cursor()
+        print("INFO:\t Database connection was successful")
+        break
+    except Exception as error:
+        print("INFO:\t Database connection fail")
+        print("INFO: \t",error)
+        time.sleep(2)
+        
 
 
 @app.get("/")
@@ -37,20 +41,25 @@ def root():
 
 @app.get("/posts")
 def get_posts() :
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 
 @app.post("/posts", status_code= status.HTTP_201_CREATED)
-def createpost(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post.dict())
-    return {"data" : my_posts}
+def create_post(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING 
+                   * """, (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data" : new_post}
 
 @app.get("/posts/{id}")
 def get_post(id : int, response : Response):
     
-    post = find_post(id)
+    cursor.execute(""" SELECT * FROM posts WHERE id= %s """,(str(id),))
+    post = cursor.fetchone() 
+    
     if not post: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with {id} was not found")
@@ -59,37 +68,37 @@ def get_post(id : int, response : Response):
 
 @app.delete ("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def Delete_post(id : int, ):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute(""" DELETE FROM posts WHERE id= %s RETURNING *""",(str(id),))
+    deleted_post = cursor.fetchone()  
+    conn.commit()
+    
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with {id} does not exsist")
-    my_posts.pop(index)
+                            detail=f"Post with {id} does not exist")
     
     
 @app.put ("/posts/{id}")
 def update_post(id : int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute(""" UPDATE posts SET title= %s, content =%s, published = %s WHERE id= %s   RETURNING *""", 
+                (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()   
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with {id} does not exsist")
-
-    post_dict = post.dict() 
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data" : post_dict}
+                            detail=f"Post with  id {id} does not exist")
+    return {"data" : updated_post}
 
 def start_server():
-    print('Starting Server...')       
+    try:
+        print('Starting Server...')       
 
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8765,
-        log_level="debug",
-        reload=True,
+        uvicorn.run(
+            "main:app",
+            reload=True,
     )
+    except Exception as e:
+        print(f"Error starting server: {e}")
 
 # To run the FastAPI app
 if __name__ == "__main__":
     start_server()
-    
